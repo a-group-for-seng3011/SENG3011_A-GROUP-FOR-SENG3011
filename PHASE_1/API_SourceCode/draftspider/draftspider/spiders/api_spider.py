72146986815f8d576abef23bc802aba7a11399b6
Meilin Huang <meilin.huang@student.unsw.edu.au>
2021-03-11 20:02:43 +1100

    report object

--- PHASE_1/API_SourceCode/draftspider/draftspider/spiders/api_spider.py
+++ PHASE_1/API_SourceCode/draftspider/draftspider/spiders/api_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from ..items import ArticleItem
+from ..items import ArticleItem, ReportItem, LocationItem
 from datetime import datetime
 
 class APISpider(scrapy.Spider):
@@ -21,19 +21,28 @@ class APISpider(scrapy.Spider):
         # date format: 2018-11-xx 17:00:xx
         extract_datetime = response.xpath("//meta[@property='article:published_time']/@content").get()
         date_str = extract_datetime[:-6]
-        date_of_publication = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+        if date_str is None:
+            date_of_publication = "xxxx-xx-xx xx:xx:xx"
+        else:
+            date_of_publication = datetime.strptime(date_str, "%Y-%m-%dT%H:%M:%S")
+
         headline = response.xpath('//title/text()').get()
         content = response.css('div.postcontent p::text').getall()[1:-1]
         main_text = ' '.join(content)
         report = '[<object::report>]'
         
         articleItem = ArticleItem()
-        # result = {articleItem}
-        # salmonella case -> temporary
+        reportItem = ReportItem()
+        locationItem = LocationItem()
+
         if 'salmonella' in headline:
+            reportItem["diseases"] = 'salmonella'
+            reportItem["syndromes"] = 'blahblah'
+
             articleItem["url"] = url
             articleItem["date_of_publication"] = date_of_publication
             articleItem["headline"] = headline
             articleItem["main_text"] = main_text
+            articleItem["report"] = [dict(reportItem)]
             # articleItem["report"] = report
             yield articleItem
