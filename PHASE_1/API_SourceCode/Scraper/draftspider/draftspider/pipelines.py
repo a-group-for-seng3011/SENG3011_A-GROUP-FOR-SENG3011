a504920334f9e6dacd7032cf778bb7a5907af6eb
Meilin Huang <meilin.huang@student.unsw.edu.au>
2021-03-25 12:35:00 +1100

    pair up country and city

--- PHASE_1/API_SourceCode/Scraper/draftspider/draftspider/pipelines.py
+++ PHASE_1/API_SourceCode/Scraper/draftspider/draftspider/pipelines.py
@@ -108,51 +108,42 @@ class LocationExtractionPipeline:
         # gets nested dictionary for cities
         cities = gc.get_cities()
         city_list = []
+        city_country = []
         for elem in cities.values():
             city_list.append(elem['name'])
+            city_country.append({elem['name']: elem['countrycode']})
 
         nlp = spacy.load("en_core_web_sm")
         doc = nlp(item['main_text'])
         country = []
         location = []
-        count = {}
+        count_country = {}
+        result = []
         for ent in doc.ents:
             if ent.label_ == 'GPE':
+                # find the matching country inside country_list
                 find_country = country_list.get(ent.text)
-                if find_country is not None and count.get(find_country) is None:
-                    count[find_country] = count.get(find_country, 0) + 1
+                if find_country is not None and count_country.get(find_country) is None:
+                    count_country[find_country] = count_country.get(find_country, 0) + 1
                     country.append(ent.text)
                 elif ent.text in city_list:
-                    location.append(ent.text)
+                    for dict_city_country in city_country:
+                        if ent.text in list(dict_city_country.keys()):
+                            location.append(dict_city_country)
+        for country_name in country:
+            iso = country_list[country_name]
+            for location_dict in location:
+                if iso in list(location_dict.values()):
+                    result.append({"country": country_name, "location": list(location_dict.keys()).pop(0)})
+            if not any(elem["country"] == country_name for elem in result):
+                result.append({"country": country_name, "location": ""})
 
-        #combine text with its label
-        # labl = {}
-        # for token in doc.ents:
-        #     labl[token.text] = token.label_
-
-        # gpe = []
-        # # TODO: why are they lists?
-        # location = []
-        # country = []
-        # for wrd,lbl in labl.items():
-        #     if lbl == "GPE":
-        #         gpe.append(wrd)
-        # for text in gpe:
-        #     summary = str(wikipedia.summary(text))
-        #     if not ('country' in summary):
-        #         location.append(text)
-        #     if ('country' in summary):
-        #         country.append(text)
-
-        # if len(location) > 0:
-        #     geolocator = Nominatim(user_agent = "geoapiExercises")
-        #     cntry = geolocator.geocode(location[0])
-    
-        # TODO: it's not complete, isn't it? 
-        # locations --(1)--------(n)-- locationItem, which means locations = [locationItem1, locationItem2, ...]
-        # locationItem = { country:<string>, location: <string> }
-        # TODO: your code here is for the 'location' attribute of a locationItem, it should be **a string** rather than a list
-        item['reports'][0]['locations'][0]['location'] = ', '.join(location) # TODO: why is this a list? or what does it stand for if you think it should be a list?
-        # TODO: your code here is for the 'country' attribute of a locationItem, it should be **a string** rather than a list
-        item['reports'][0]['locations'][0]['country'] = ', '.join(country) # TODO: why is this a list? or what does it stand for if you think it should be a list?
+        i = 0
+        for e in result:
+            if i == 0:
+                item['reports'][0]['locations'][i]['location'] = e['location']
+                item['reports'][0]['locations'][i]['country'] = e['country']
+                i = 1
+            else:
+                item['reports'][0]['locations'].append(e)
         return item
